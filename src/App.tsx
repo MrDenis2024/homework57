a454fd4d2734ce80a7f5a6cbb03fb858d5023325
Khrunev Denis <deniskhrunev@gmail.com>
2024-06-06 15:19:32 +0600

    Созданы компоненты Users и UserItem которые отрисовывют всех пользователей на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import {useState} from 'react';
 import './App.css';
+import {User} from './types';
+import Users from './components/Users/Users';
 
 const App = () => {
+  const [users, setUsers] = useState<User[]>([
+    {id: '1', name: 'Den', active: true, email: 'den@mail.ru', role: 'admin'},
+    {id: '2', name: 'Den', active: false, email: 'den@mail.ru', role: 'admin'}
+  ]);
+
   return (
-    <>
-    </>
+    <div className='row mt-2 justify-content-between'>
+      <div className='col-6'>
+
+      </div>
+      <div className='col-3'>
+        <Users users={users} />
+      </div>
+    </div>
   );
 };
 
